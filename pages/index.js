0d5595e710c75131c6fbe5af1943d4232fdfc7d1
Adam Iverson <aciverso@gmail.com>
2023-04-29 21:00:02 -0500

    pages in

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return <div>
+    <h1>Welcome to Next.js!</h1>
+    <p>read my <Link href='/posts/first-post'>first post</Link></p>
+
+  </div>
+
 }
 
 export default HomePage
