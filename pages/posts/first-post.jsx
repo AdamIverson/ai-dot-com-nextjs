7e0549c6e56632f847e22b55d2280eb3484d0566
Adam Iverson <aciverso@gmail.com>
2023-04-30 07:54:12 -0500

    photos functional

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
+import pic from '/public/next.svg'
+import flower from '/public/assets/56810001.jpg'
 
 export default function FirstPost() {
   return (
@@ -6,7 +9,8 @@ export default function FirstPost() {
       <h1>First Post</h1>
       <Link href='/'>go back</Link>
       <br />
-      {/* <Profile /> */}
+      <Image src={pic} height={400} width={400} alt=''/>
+      <Image src={flower} height={400} width={500} alt=''/>
     </div>
   )
 }
